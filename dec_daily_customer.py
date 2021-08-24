2f7545c3099629259875782bb318833bb17c0463
ahmdrzk30 <ahmdrzk.30@gmail.com>
2021-08-24 19:00:00 +0700

    comment

--- dec_daily_customer.py
+++ dec_daily_customer.py
@@ -6,10 +6,8 @@ dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_r
 
 dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
 
-dataset['gmv'] = dataset['item_price'] * dataset['quantity']
-
 plt.figure(figsize=(10, 5))
-dataset[dataset['order_month']=='2019-12'].groupby(['order_date'])['customer_id'].nunique().plot(color='red', marker='*', linewidth=2)
+dataset[dataset['order_month']=='2019-12'].groupby(['order_date'])['customer_id'].nunique().plot(color='red', marker='.', linewidth=2)
 plt.title('Daily Number of Customers - December 2019', loc='center', pad=30, fontsize=20, color='black')
 plt.xlabel('Order Date', fontsize=15, color='blue')
 plt.ylabel('Number of Customers', fontsize=15, color='blue')
